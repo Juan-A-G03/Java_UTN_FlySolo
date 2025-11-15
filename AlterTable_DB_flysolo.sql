688e7b0b83c0bc6c005b8d06856f7a7c309c4cc3
Juan Augusto <galluccij15@gmail.com>
2025-11-14 23:57:41 -0300

    correccion Alter

--- AlterTable_DB_flysolo.sql
+++ AlterTable_DB_flysolo.sql
@@ -1,17 +1,13 @@
 USE flysolo;
 
--- 1) Agregar columna 'password' como NULL temporalmente
 ALTER TABLE users
   ADD COLUMN password VARCHAR(128) NULL AFTER email;
 
--- 2) Completar contraseñas de ejemplo (en texto plano, solo para pruebas)
---    Ajusta los valores si lo deseas.
 UPDATE users SET password = 'admin123'   WHERE email = 'mothma@rebel.example';
 UPDATE users SET password = 'admin123'   WHERE email = 'officer@imperial.example';
 UPDATE users SET password = 'leia123usersusers'    WHERE email = 'leia@rebel.example';
 UPDATE users SET password = 'han123'     WHERE email = 'han@neutral.example';
 UPDATE users SET password = 'trader123'  WHERE email = 'trader@neutral.example';
 
--- 3) Hacer la columna NOT NULL una vez que todos tengan valor
 ALTER TABLE users
   MODIFY password VARCHAR(128) NOT NULL;
